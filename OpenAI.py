77bc376c7c1514057a023a5a64633d430862c250
DillTheBeast <dillonmaltese@gmail.com>
2023-11-13 09:05:44 -0500

    Update OpenAI.py

--- OpenAI.py
+++ OpenAI.py
@@ -1,31 +1,8 @@
-import openai as ai
+import os 
+os.environ["OPENAI_API_KEY"] = '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'
 
-ai.api_key = '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'
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
+documents = SimpleDirectoryReader('/Users/tarakram/Documents/Chatbot/data').load_data()
+print(documents)
 
-def chat_with_chatgpt(prompt, model="gpt-3.5-turbo"):
-    response = ai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        temperature=0.5,
-    )
-
-    message = response.choices[0].text.strip()
-    return message
-
-def chat_with_gpt():
-    print("Chatbot: Hello! Ask me anything or type 'exit' to end the conversation.")
-
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == 'exit':
-            print("Chatbot: Goodbye!")
-            break
-
-        prompt = f"You: {user_input}\nChatbot:"
-        response = chat_with_chatgpt(prompt)
-        print(f"Chatbot: {response}")
-
-if __name__ == "__main__":
-    chat_with_gpt()
+index = GPTVectorStoreIndex.from_documents(documents)
