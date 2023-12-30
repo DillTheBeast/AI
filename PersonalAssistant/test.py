393089308c3819e50e2db519d7847c8114df2683
DillTheBeast <dillonmaltese@gmail.com>
2023-12-30 17:48:25 -0500

    Update test.py

--- PersonalAssistant/test.py
+++ PersonalAssistant/test.py
@@ -1,85 +1,27 @@
-# import sys module 
-import pygame 
-import sys 
-# pygame.init() will initialize all 
-# imported module 
-pygame.init() 
+import PySimpleGUI as sg
 
-clock = pygame.time.Clock() 
+def main():
+    recording = False
+    layout = [
+        [sg.Text("Welcome to your personal Assistant", text_color='white', background_color='gray', justification='center', font=('Helvetica', 16), key='welcome_text', expand_x=True)],
+        [sg.Multiline('', size=(50, 6), key='input_text', pad=((0, 0), (10, 0)), font=('Helvetica', 20)), sg.Button("🎤", size=(4, 1.05), font=('Helvetica', 40), key='microphone_button', pad=((5, 0), (10, 0)), button_color=('black', 'lightgray'), enable_events=True)],
+    ]
 
-# it will display on screen 
-screen = pygame.display.set_mode([600, 500]) 
+    window = sg.Window("Personal Assistant", layout, size=(800, 600), background_color='gray')
 
-# basic font for user typed 
-base_font = pygame.font.Font(None, 32) 
-user_text = '' 
+    while True:
+        event, values = window.read()
 
-# create rectangle 
-input_rect = pygame.Rect(200, 200, 140, 32) 
+        if event == sg.WINDOW_CLOSED:
+            break
+        elif event == 'microphone_button':
+            recording = not recording
+            if recording:
+                window['microphone_button'].update(button_color=('lightgray', 'red'))
+            else:
+                window['microphone_button'].update(button_color=('red', 'lightgray'))
 
-# color_active stores color(lightskyblue3) which 
-# gets active when input box is clicked by user 
-color_active = pygame.Color('lightskyblue3') 
+    window.close()
 
-# color_passive store color(chartreuse4) which is 
-# color of input box. 
-color_passive = pygame.Color('chartreuse4') 
-color = color_passive 
-
-active = False
-
-while True: 
-	for event in pygame.event.get(): 
-
-	# if user types QUIT then the screen will close 
-		if event.type == pygame.QUIT: 
-			pygame.quit() 
-			sys.exit() 
-
-		if event.type == pygame.MOUSEBUTTONDOWN: 
-			if input_rect.collidepoint(event.pos): 
-				active = True
-			else: 
-				active = False
-
-		if event.type == pygame.KEYDOWN: 
-
-			# Check for backspace 
-			if event.key == pygame.K_BACKSPACE: 
-
-				# get text input from 0 to -1 i.e. end. 
-				user_text = user_text[:-1] 
-
-			# Unicode standard is used for string 
-			# formation 
-			else: 
-				user_text += event.unicode
-	
-	# it will set background color of screen 
-	screen.fill((255, 255, 255)) 
-
-	if active: 
-		color = color_active 
-	else: 
-		color = color_passive 
-		
-	# draw rectangle and argument passed which should 
-	# be on screen 
-	pygame.draw.rect(screen, color, input_rect) 
-
-	text_surface = base_font.render(user_text, True, (255, 255, 255)) 
-	
-	# render at position stated in arguments 
-	screen.blit(text_surface, (input_rect.x+5, input_rect.y+5)) 
-	
-	# set width of textfield so that text cannot get 
-	# outside of user's text input 
-	input_rect.w = max(100, text_surface.get_width()+10) 
-	
-	# display.flip() will update only a portion of the 
-	# screen to updated, not full area 
-	pygame.display.flip() 
-	
-	# clock.tick(60) means that for every second at most 
-	# 60 frames should be passed. 
-	clock.tick(60) 
+if __name__ == "__main__":
+    main()
