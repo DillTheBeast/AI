7d06680c9d028ebdf4c138e351de4c9d463f575e
DillTheBeast <dillonmaltese@gmail.com>
2023-12-30 18:50:43 -0500

    as

--- PersonalAssistant/DesktopApp.py
+++ PersonalAssistant/DesktopApp.py
@@ -1,118 +1,88 @@
-import pygame
-from pygame.locals import *
-from Button import Button
+import PySimpleGUI as sg
+import speech_recognition as sr
+import audioop
+import wave
+from pydub import AudioSegment
+import threading
 
-# Initialize pygame
-pygame.init()
+# Initialize the recognizer and the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source, duration=0.2)
 
-# Set up pygame window
-WIDTH, HEIGHT = 800, 600
-screen_size = (WIDTH, HEIGHT)
-screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption("Personal Assistant")
-font = pygame.font.Font(None, 32)
-userInput = ''
+# Function to toggle recording state
+def toggle_recording():
+    global recording_active
+    recording_active = not recording_active
 
-# Set the initial background color to black
-background_color = (0, 0, 0)
-screen.fill(background_color)
+def speech_to_text(audio):
+    try:
+        # Using Google to recognize audio
+        my_text = r.recognize_google(audio)
+        my_text = my_text.lower()
+        return my_text
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        return None
+    except sr.UnknownValueError:
+        print("Unknown error occurred")
+        return None
 
-# Define colors
-white = (255, 255, 255)
-black = (0, 0, 0)
-inputBox = pygame.Rect(30, 60, 500, 32)
-inputBoxActive = (200, 200, 200)
-inputBoxPassive = white
-currentInputBox = inputBoxPassive
+def save_to_mp3(audio_data, filename='audio.mp3'):
+    wav_filename = 'audio.wav'
+    with wave.open(wav_filename, 'wb') as wf:
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(44100)
+        wf.writeframes(audio_data)
 
-# Draw a white microphone
-mic_surface = pygame.Surface((200, 100), pygame.SRCALPHA)
-pygame.draw.rect(mic_surface, (0, 0, 0, 0), (50, 5, 20, 40))
-pygame.draw.circle(mic_surface, black, (25, 25), 20, 2)
-pygame.draw.circle(mic_surface, black, (25, 25), 20)
-pygame.draw.rect(mic_surface, black, (15, 47, 20, 50))
+    sound = AudioSegment.from_wav(wav_filename)
+    sound.export(filename, format="mp3")
+    return filename
 
-# Render text on top of the background
-font = pygame.font.Font(None, 36)
-text_surface = font.render("Welcome to Your Personal Assistant", True, (255, 255, 255))  # Set text color to white
-text_rect = text_surface.get_rect(center=(WIDTH // 2, 20))
-screen.blit(text_surface, text_rect)
+def record_audio(window):
+    global recording_active
+    while recording_active:
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            audio_data = audioop.tomono(audio.get_raw_data(), audio.sample_width, 0.5, 0.5)  # Convert stereo to mono
+            save_to_mp3(audio_data)
+            answer = speech_to_text(audio)
+            if answer:
+                window.write_event_value('update_text', answer[0].upper() + ''.join([answer[i] for i in range(1, len(answer))]) + ". ")
 
-# Create a button with the white microphone image
-button_rect_white = pygame.Rect((WIDTH - 100, 50, 50, 105))
-button_white_mic = Button(button_rect_white, mic_surface, (30, 30, 30), (60, 60, 60), lambda: toggle_mic_color())
+def main():
+    global recording_active
+    recording_active = False
+    recorded_text_list = []
 
-# Initialize mic color toggle variable
-mic_color_white = True
+    layout = [
+        [sg.Text("Welcome to your personal Assistant", text_color='white', background_color='gray', justification='center', font=('Helvetica', 16), key='welcome_text', expand_x=True)],
+        [sg.Multiline('', size=(50, 6), key='input_text', font=('Helvetica', 20)), sg.Button("🎤", size=(20, 1.2), font=('Helvetica', 20), key='microphone_button', button_color=('black', 'lightgray'), enable_events=True)],
+    ]
 
-def toggle_mic_color():
-    global mic_color_white
-    mic_color_white = not mic_color_white
-    type_ = black if mic_color_white else white
+    window = sg.Window("Personal Assistant", layout, size=(800, 600), background_color='gray')
 
-    pygame.draw.circle(mic_surface, type_, (25, 25), 20)
-    pygame.draw.rect(mic_surface, type_, (15, 47, 20, 50))
+    while True:
+        event, values = window.read(timeout=500)  # Reduced the update frequency to 500 milliseconds
 
-# Update the display
-pygame.display.flip()
+        if event == sg.WINDOW_CLOSED:
+            break
+        elif event == 'microphone_button':
+            toggle_recording()
+            if recording_active:
+                window['microphone_button'].update(button_color=('lightgray', 'red'))
+                # Start a new thread for recording
+                recording_thread = threading.Thread(target=record_audio, args=(window,), daemon=True)
+                recording_thread.start()
+            else:
+                window['microphone_button'].update(button_color=('red', 'lightgray'))
 
-# Event loop
-running = True
-active = False
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if active:
-                if event.key == pygame.K_RETURN:
-                    # Process the user input
-                    print(userInput)
-                    userInput = ''
-                elif event.key == pygame.K_BACKSPACE:
-                    # Remove the last character from the user input
-                    userInput = userInput[:-1]
-                else:
-                    # Append the pressed key to the user input
-                    userInput += event.unicode
+        elif event == 'update_text':
+            # Event received from the recording thread, update the text in the GUI
+            window['input_text'].update(value=values[event], append=True)
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if inputBox.collidepoint(event.pos):
-                # Button clicked
-                print("Button clicked")
+    window.close()
 
-    # Calculate the width of the text surface
-    text_surface = font.render(userInput, True, black)
-    text_width = text_surface.get_width()
-
-    # Update the width and height of the input box
-    input_box_width = max(200, text_width + 10)
-    input_box_height = 40
-
-    # Update the input box position
-    input_box_x = (WIDTH - input_box_width) // 2
-    input_box_y = (HEIGHT - input_box_height) // 2
-
-    # Update the button position
-    button_x = input_box_x + input_box_width + 20
-    button_y = input_box_y
-    button_width = 80
-    button_height = input_box_height
-
-    # Draw the input box
-    pygame.draw.rect(screen, currentInputBox, (input_box_x, input_box_y, input_box_width, input_box_height))
-
-    # Draw the text surface inside the input box
-    screen.blit(text_surface, (input_box_x + 5, input_box_y + 5))
-
-    # Draw the button
-    pygame.draw.rect(screen, button_color, (button_x, button_y, button_width, button_height))
-    button_text = font.render("Submit", True, black)
-    screen.blit(button_text, (button_x + 10, button_y + 10))
-
-    pygame.display.update()
-    clock.tick(30)
-
-# Quit pygame
-pygame.quit()
+if __name__ == "__main__":
+    main()
--- PersonalAssistant/test.py
+++ PersonalAssistant/test.py
@@ -1,28 +1,86 @@
 import PySimpleGUI as sg
+import speech_recognition as sr
+import audioop
+import wave
+from pydub import AudioSegment
+import threading
 
+# Initialize the recognizer and the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source, duration=0.2)
+
+# Function to toggle recording state
+def toggle_recording():
+    global recording_active
+    recording_active = not recording_active
+
+def speech_to_text(audio):
+    try:
+        # Using Google to recognize audio
+        my_text = r.recognize_google(audio)
+        my_text = my_text.lower()
+        return my_text
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        return None
+    except sr.UnknownValueError:
+        print("Unknown error occurred")
+        return None
+
+def save_to_mp3(audio_data, filename='audio.mp3'):
+    wav_filename = 'audio.wav'
+    with wave.open(wav_filename, 'wb') as wf:
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(44100)
+        wf.writeframes(audio_data)
+
+    sound = AudioSegment.from_wav(wav_filename)
+    sound.export(filename, format="mp3")
+    return filename
+
+def record_audio(window):
+    global recording_active
+    while recording_active:
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            audio_data = audioop.tomono(audio.get_raw_data(), audio.sample_width, 0.5, 0.5)  # Convert stereo to mono
+            save_to_mp3(audio_data)
+            answer = speech_to_text(audio)
+            if answer:
+                # Use PySimpleGUI's update method for threaded updates
+                window['input_text'].update(value=answer[0].upper() + ''.join([answer[i] for i in range(1, len(answer))]) + ". ", append=True)
 def main():
-    recording = False
+    global recording_active
+    recording_active = False
+    recorded_text_list = []
+
     layout = [
         [sg.Text("Welcome to your personal Assistant", text_color='white', background_color='gray', justification='center', font=('Helvetica', 16), key='welcome_text', expand_x=True)],
-        [sg.Multiline('', size=(50, 6), key='input_text', pad=((0, 0), (10, 0)), font=('Helvetica', 20)), sg.Button("🎤", size=(4, 1.05), font=('Helvetica', 40), key='microphone_button', pad=((5, 0), (10, 0)), button_color=('black', 'lightgray'), enable_events=True)],
+        [sg.Multiline('', size=(50, 6), key='input_text', font=('Helvetica', 20)), sg.Button("🎤", size=(20, 1.2), font=('Helvetica', 20), key='microphone_button', button_color=('black', 'lightgray'), enable_events=True)],
     ]
 
     window = sg.Window("Personal Assistant", layout, size=(800, 600), background_color='gray')
 
     while True:
-        event, values = window.read(timeout=100)
+        event, values = window.read(timeout=500)  # Reduced the update frequency to 500 milliseconds
 
         if event == sg.WINDOW_CLOSED:
             break
         elif event == 'microphone_button':
-            recording = not recording
-            if recording:
+            toggle_recording()
+            if recording_active:
                 window['microphone_button'].update(button_color=('lightgray', 'red'))
-                currentText = values['input_text']
-                newText = currentText + 'test\n'
-                window['input_text'].update(value=newText)
+                # Start a new thread for recording
+                recording_thread = threading.Thread(target=record_audio, args=(window,), daemon=True)
+                recording_thread.start()
             else:
                 window['microphone_button'].update(button_color=('red', 'lightgray'))
+
+        elif event == 'update_text':
+            # Event received from the recording thread, update the text in the GUI
+            window['input_text'].update(value=values[event], append=True)
 
     window.close()
 
